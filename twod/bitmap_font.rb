18d0ba1e1578a8566ef9f0c328f7597bd60d28d2
Corey Powell <mistdragon100@gmail.com>
2014-09-20 10:56:09 -0500

    twod: Updated bitmap_font to support latest RenderContainer methods

--- twod/bitmap_font.rb
+++ twod/bitmap_font.rb
@@ -1,18 +1,12 @@
 class BitmapFont < Moon::RenderContainer
   attr_reader :string
-  def string=(n)
-    @string = n != nil ? n.to_s : nil
-    refresh_size
-  end
-
   attr_accessor :bold
   attr_accessor :color
 
-  ###
+  ##
   # Current font cell size is fixed at 8x8
   # @param [String] filename font file name
   # @param [String] string initial ASCII string value
-  ###
   def initialize(filename, string="")
     super()
     @color = Vector4.new 1.0, 1.0, 1.0, 1.0
@@ -21,30 +15,32 @@ class BitmapFont < Moon::RenderContainer
     self.string = string
   end
 
+  ##
+  # @param [String] n
+  def string=(n)
+    @string = (n && n.to_s) || nil
+    refresh_size
+  end
+
+  ##
+  #
   def refresh_size
     @cached_width = @string.size * @cell_size[0]
     @cached_height = (@string.count("\n") + 1) * @cell_size[1]
+
+    self.width = @cached_width
+    self.height = @cached_height
   end
 
-  def width
-    @cached_width
-  end
-
-  def height
-    @cached_height
-  end
-
-  ###
+  ##
   # @param [Integer] x
   # @param [Integer] y
   # @param [Integer] z
-  ###
-  def render(x, y, z)
+  def render_elements(x, y, z, options={})
     if @string
       offset = @bold ? 256 : 0
       row = 0
       col = 0
-      px, py, pz = *@position
 
       @string.bytes.each_with_index do |byte, i|
         if byte.chr == "\n"
@@ -52,9 +48,9 @@ class BitmapFont < Moon::RenderContainer
           row += 1
           next
         end
-        @spritesheet.render px + x + col * @cell_size[0],
-                            py + y + row * @cell_size[1],
-                            pz + z,
+        @spritesheet.render x + col * @cell_size[0],
+                            y + row * @cell_size[1],
+                            z,
                             byte + offset,
                             color: @color
         col += 1
