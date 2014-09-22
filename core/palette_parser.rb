75a36c1e8aae75a3ed8c2d5e86f19fd999c58fca
Corey Powell <mistdragon100@gmail.com>
2014-09-22 16:44:12 -0500

    core: PalleteParser refactored hex parsing

--- core/palette_parser.rb
+++ core/palette_parser.rb
@@ -1,32 +1,28 @@
 module PaletteParser
-
   class FormatError < StandardError
   end
 
-  def self.load_palette_hex_rgb(colors)
+  def self.load_palette_hex(colors, bits, format_rgx, format_str)
+    bpc = (bits / 3).to_i # bits per channel
+    bitmask = 2 ** bpc - 1
     colors.each_with_object({}) do |a, hash|
       key, hex = *a
-      matchdata = hex.match(/[0-9a-fA-F]{3}/)
-      raise FormatError, "invalid hex format (#{hex}) expected (RGB)" unless matchdata
+      matchdata = hex.match(format_rgx)
+      raise FormatError, "invalid hex format (#{hex}) expected (#{format_str})" unless matchdata
       value = matchdata[0].to_i(16)
-      r = (value >> 8) & 15
-      g = (value >> 4) & 15
-      b = value & 15
-      hash[key] = Moon::Vector4.new(r/15.0, g/15.0, b/15.0, 1.0)
+      r = (value >> (bpc * 2)) & bitmask
+      g = (value >> bpc) & bitmask
+      b = value & bitmask
+      hash[key] = Moon::Vector4.new(r/bitmask.to_f, g/bitmask.to_f, b/bitmask.to_f, 1.0)
     end
   end
 
+  def self.load_palette_hex_rgb(colors)
+    load_palette_hex(colors, 12, /[0-9a-fA-F]{3}/, "RGB")
+  end
+
   def self.load_palette_hex_rrggbb(colors)
-    colors.each_with_object({}) do |a, hash|
-      key, hex = *a
-      matchdata = hex.match(/[0-9a-fA-F]{6}/)
-      raise FormatError, "invalid hex format (#{hex}) expected (RRGGBB)" unless matchdata
-      value = matchdata[0].to_i(16)
-      r = (value >> 16) & 255
-      g = (value >> 8) & 255
-      b = value & 255
-      hash[key] = Moon::Vector4.new(r/255.0, g/255.0, b/255.0, 1.0)
-    end
+    load_palette_hex(colors, 24, /[0-9a-fA-F]{6}/, "RRGGBB")
   end
 
   def self.load_palette_array_rgb(colors)
@@ -81,5 +77,4 @@ module PaletteParser
       raise FormatError, "unsupported palette format #{format}"
     end
   end
-
 end
