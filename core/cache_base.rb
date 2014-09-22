a501625a497e43b6b74146c4692aa3511a4017b4
Corey Powell <mistdragon100@gmail.com>
2014-09-22 17:07:12 -0500

    core: CacheBase, removed deprecated methods

--- core/cache_base.rb
+++ core/cache_base.rb
@@ -45,46 +45,6 @@ module Moon
     end
 
     ##
-    # @deprecated
-    def __cache_load__(branch_name, *args_org, &block)
-      name, *args = *args_org
-      key = [name, *args]
-
-      debug { |l| l.puts "[#{@name}:#{branch_name}] GET #{name.inspect}, #{args.join(", ")}" }
-
-      case loader = self.class.loader[branch_name]
-      when Proc # dynamic loader
-        (@cache[branch_name] ||= {})[key] ||= loader.(name, *args)
-      when Hash # fixed loader
-        func = loader[name]
-        unless func
-          raise RuntimeError,
-                "[#{@name}:#{branch_name}] ERR #{name.inspect}, #{args.join(", ")} (no loader)"
-        end
-        (@cache[branch_name] ||= {})[key] ||= func.(*args)
-      else
-        raise IndexError,
-              "[#{@name}:#{branch_name}] ERR #{name.inspect}, #{args.join(", ")} (no loader)"
-      end
-    end
-
-    ##
-    # @param [Symbol] branch_name
-    # @return [Symbol]  branch_name
-    # @deprecated
-    def self.branch(branch_name)
-      STDERR.puts "warning: CacheBase.branch is deprecated, use .cache instead"
-
-      (@loader ||= {})[branch_name] = yield
-
-      define_method(branch_name) do |*args_org, &block|
-        __cache_load__(branch_name, *args_org, &block)
-      end
-
-      branch_name
-    end
-
-    ##
     # @param [Symbol] method_name
     def self.cache(method_name)
       alias_method "load_#{method_name}", method_name
