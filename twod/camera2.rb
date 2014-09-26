3a6be273cc59e768b8392ec72f16fa070266a633
Corey Powell <mistdragon100@gmail.com>
2014-09-26 18:00:32 -0500

    twod: Fixed Camera*#view

--- twod/camera2.rb
+++ twod/camera2.rb
@@ -1,4 +1,8 @@
 class Camera2 < CameraBase
   field :position, type: Moon::Vector2, default: proc{|t|t.new}
   field :tilesize, type: Moon::Vector2, default: proc{|t|t.new(32, 32)}
+
+  def view
+    @position + @viewport.xy
+  end
 end
--- twod/camera3.rb
+++ twod/camera3.rb
@@ -1,4 +1,8 @@
 class Camera3 < CameraBase
   field :position, type: Moon::Vector3, default: proc{|t|t.new}
   field :tilesize, type: Moon::Vector3, default: proc{|t|t.new(32, 32, 32)}
+
+  def view
+    @position + @viewport.xyz
+  end
 end
