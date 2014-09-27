e9e81fbcdf1986e271462b682b20c29b37219f44
Corey Powell <mistdragon100@gmail.com>
2014-09-27 13:55:02 -0500

    duck: Vector arithmetic operations

--- duck/moon/vector2.rb
+++ duck/moon/vector2.rb
@@ -24,5 +24,60 @@ module Moon
         dm_set x: 0.0, y: 0.0, &block
       end
     end
+
+    def +(other)
+      ox, oy = *self.class.extract(other)
+      self.class.new(x + ox, y + oy)
+    end
+
+    def -(other)
+      ox, oy = *self.class.extract(other)
+      self.class.new(x - ox, y - oy)
+    end
+
+    def *(other)
+      ox, oy = *self.class.extract(other)
+      self.class.new(x * ox, y * oy)
+    end
+
+    def /(other)
+      ox, oy = *self.class.extract(other)
+      self.class.new(x / ox, y / oy)
+    end
+
+    def -@
+      self.class.new(-x, -y)
+    end
+
+    def self.extract(obj)
+      case obj
+      when Numeric
+        return obj, obj, obj
+      when Vector2
+        return *obj
+      when Array
+        case obj.size
+        when 1
+          x, y = *obj.first
+        when 2
+          x, y = *obj
+        else
+          raise ArgumentError, "expected Array of size 1 or 2"
+        end
+        return x, y
+      when Hash
+        return obj.fetch(:x), obj.fetch(:y)
+      else
+        raise TypeError, "wrong argument type (expected Array, Hash, Numeric or Vector2)"
+      end
+    end
+
+    def self.[](*args)
+      if args.size > 1
+        new(*extract(args))
+      else
+        new(*extract(args.first))
+      end
+    end
   end
 end
--- duck/moon/vector4.rb
+++ duck/moon/vector4.rb
@@ -34,5 +34,75 @@ module Moon
         dm_set x: 0.0, y: 0.0, z: 0.0, w: 0.0, &block
       end
     end
+
+    def +(other)
+      ox, oy, oz, ow = *self.class.extract(other)
+      self.class.new(x + ox, y + oy, z + oz, w + ow)
+    end
+
+    def -(other)
+      ox, oy, oz, ow = *self.class.extract(other)
+      self.class.new(x - ox, y - oy, z - oz, w - ow)
+    end
+
+    def *(other)
+      ox, oy, oz, ow = *self.class.extract(other)
+      self.class.new(x * ox, y * oy, z * oz, w * ow)
+    end
+
+    def /(other)
+      ox, oy, oz, ow = *self.class.extract(other)
+      self.class.new(x / ox, y / oy, z / oz, w / ow)
+    end
+
+    def -@
+      self.class.new(-x, -y, -z, -w)
+    end
+
+    def self.extract(obj)
+      case obj
+      when Numeric
+        return obj, obj, obj, obj
+      when Vector4
+        return *obj
+      when Array
+        case obj.size
+        when 1
+          x, y, z = *obj.first
+        when 2, 3
+          params = []
+          for item in obj
+            case item
+            when Numeric
+              params << item
+            when Vector2
+              params.concat(obj.to_a)
+            when Vector3
+              params.concat(obj.to_a)
+            else
+              raise TypeError, "expected Numeric, Vector2 or Vector3"
+            end
+          end
+          x, y, z, w = *params
+        when 4
+          x, y, z, w = *obj
+        else
+          raise ArgumentError, "expected Array of size 1, 2, 3, or 4"
+        end
+        return x, y, z, w
+      when Hash
+        return obj.fetch(:x), obj.fetch(:y), obj.fetch(:z)
+      else
+        raise TypeError, "wrong argument type (expected Array, Hash, Numeric or Vector3)"
+      end
+    end
+
+    def self.[](*args)
+      if args.size > 1
+        new(*extract(args))
+      else
+        new(*extract(args.first))
+      end
+    end
   end
 end
