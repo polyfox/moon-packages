29b7433bdb9ff0fc94acd10c826dec934cc86757
Corey Powell <mistdragon100@gmail.com>
2014-11-21 14:25:51 -0500

    core: added Module abstract_* methods

--- core/core_ext/module.rb
+++ core/core_ext/module.rb
@@ -13,5 +13,34 @@ class Module
       paths.shift
     end
     paths.inject(top) { |klass, name| klass.__const_get__(name) }
+
+  ##
+  # @param [Symbol] method_name
+  def abstract(method_name)
+    define_method method_name do |*|
+      fail AbstractMethodError.new(method_name)
+    end
+  end
+
+  ##
+  # @param [Symbol] method_name
+  def abstract_attr_writer(method_name)
+    abstract "#{method_name}="
+  end
+
+  ##
+  # @param [Symbol] method_name
+  def abstract_attr_reader(method_name)
+    abstract method_name
+  end
+
+  ##
+  # @param [Symbol] method_name
+  def abstract_attr_accessor(method_name)
+    abstract_attr_writer(method_name)
+    abstract_attr_reader(method_name)
+    method_name
+  end
+
   end
 end
