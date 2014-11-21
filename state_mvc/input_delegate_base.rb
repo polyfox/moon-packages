478eaf2eeefc540f01b6b65be060a46efb22bc10
Corey Powell <mistdragon100@gmail.com>
2014-11-21 14:29:14 -0500

    state_mvc: ruby style guide (this is really getting tiring)

--- state_mvc/input_delegate_base.rb
+++ state_mvc/input_delegate_base.rb
@@ -1,14 +1,23 @@
+# :nodoc
 class State
+  ##
+  #
   class InputDelegateBase
+    ##
+    # @param [State::ControllerBase] controller
     def initialize(controller)
       @controller = controller
       init
     end
 
+    ##
+    # @abstract
     def init
       #
     end
 
+    ##
+    # @param [Moon::Input::Observer, Moon::Eventable] input
     def register(input)
       #
     end
--- state_mvc/view_base.rb
+++ state_mvc/view_base.rb
@@ -1,16 +1,34 @@
+##
+# :nodoc:
 class State
+  ##
+  #
   class ViewBase < Moon::RenderContainer
+    # @return [State::ModelBase]
     attr_accessor :model
 
-    def init
+    ##
+    # @param [Hash<Symbol, Object>] options
+    private def init_from_options(options)
+      super
+      @model = options.fetch(:model, nil)
+    end
+
+    ##
+    #
+    private def init
       super
       init_view
     end
 
+    ##
+    # @abstract
     def start
-      # called by controller
+      # called by State::ControllerBase
     end
 
+    ##
+    # @abstract
     def init_view
       #
     end
