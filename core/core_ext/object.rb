be923631eabb48f2c41c99647800731518f29069
Corey Powell <mistdragon100@gmail.com>
2014-11-21 14:24:06 -0500

    core: added Object#dotsend

--- core/core_ext/object.rb
+++ core/core_ext/object.rb
@@ -14,4 +14,27 @@ class Object
       yield self
     end
   end
+
+  # @param [Array<String, Symbol>] paths
+  def paths_send(paths, *args, &block)
+    pths = paths.dup
+    last = pths.pop
+    pths.reduce(self) { |r, meth| r.send(meth) }.send(last, *args, &block)
+  end
+
+  #
+  # @param [String, Symbol, Array<String, Symbol>] dotsend
+  # @eg
+  #   obj.dotsend('position.x')
+  #   obj.dotsend('map.data.xsize')
+  def dotsend(path, *args, &block)
+    if path.is_a?(Symbol)
+      send(path, *args, &block)
+    elsif path.is_a?(Array)
+      paths_send(path, *args, &block)
+    else
+      paths = path.split('.')
+      paths_send(paths, *args, &block)
+    end
+  end
 end
