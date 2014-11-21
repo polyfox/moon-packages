a79dd7883f0eb246dd780e93fdaf251426158425
Corey Powell <mistdragon100@gmail.com>
2014-11-21 14:27:59 -0500

    core: simplified transitionable using Object#dotsend

--- core/mixins/transitionable.rb
+++ core/mixins/transitionable.rb
@@ -3,57 +3,47 @@ module Moon
     ### mixins
     include TransitionHost
 
-    ###
     # @param [String] attribute
     #   @example "color"
     #   @example "position.x"
     # @param [Object] value  target value
     # @param [Numeric] duration  in seconds
-    ###
-    def transition(attribute, value, duration=0.20, easer=Easing::Linear)
-      rolling = attribute.to_s.split(".")
-      if rolling.size > 1
-        src = rolling.inject(self) { |obj, param| obj.send(param) }
-        setter = "#{rolling.pop}="
-        add_transition(src, value, duration, easer) do |v|
-          rolling.inject(self) { |obj, param| obj.send(param) }.send(setter, v)
-        end
-      else
-        add_transition(send(attribute), value, duration, easer) do |v|
-          send("#{attribute}=", v)
-        end
+    def transition(attribute, value, duration, easer = nil)
+      easer ||= Easing::Linear
+      src = dotsend(attribute)
+      setter = "#{attribute}="
+      add_transition(src, value, duration, easer) do |v|
+        dotsend(setter, v)
       end
     end
 
-    ###
     # Add a keyed transition
     #   A keyed transition will only have 1 active transition per attribute
-    #   However multiple transition can still operate on the attribute if
+    #   However multiple transitions can still operate on the attribute if
     #   they where not keyed.
     # @param [String] attribute
     #   @example "color"
     #   @example "position.x"
     # @param [Object] value  target value
     # @param [Numeric] duration  in seconds
-    ###
-    def key_transition(attribute, value, duration=0.15)
+    def key_transition(attribute, value, duration)
       @key_transition ||= {}
-      remove_transition(@key_transition[attribute]) if @key_transition.key?(attribute)
-      t = transition(attribute, value, duration)
-      t.key = attribute
-      @key_transition[attribute] = t
+      if @key_transition.key?(attribute)
+        remove_transition(@key_transition[attribute])
+      end
+      key = attribute
+      t = @key_transition[key] = transition(attribute, value, duration)
+      t.key = key
       t
     end
 
-    ###
     # @param [Array<Transitions>] transitions
     # @return [Void]
-    ###
     def remove_transitions(transitions)
       if @key_transition
         transitions.each { |t| @key_transition.delete(t.key) if t.key }
       end
-      super transitions
+      super
     end
   end
 end
