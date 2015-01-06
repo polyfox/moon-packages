dfe129c758a2c35b3c794c462388460a73d1148a
Corey Powell <mistdragon100@gmail.com>
2015-01-05 19:35:40 -0500

    std: Ported MoonTetris Input::trigger

--- std/input.rb
+++ std/input.rb
@@ -1,5 +1,21 @@
 module Moon
   module Input
+    @channels = []
+
+    def self.register(channel)
+      @channels.push channel
+    end
+
+    def self.unregister(channel)
+      @channels.delete channel
+    end
+
+    def self.trigger(event)
+      @channels.each do |channel|
+        channel.trigger event
+      end
+    end
+
     def self.on_key(key, _, action, mods)
       trigger KeyboardEvent.new(key, action, mods)
     end
