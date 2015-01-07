717ac0b25850dc663bd3c2acd368db76b1333a4c
Corey Powell <mistdragon100@gmail.com>
2015-01-07 13:32:38 -0500

    test: micro spec now prints the test name and whether it passed or not in color

--- lib/moon/modules/test/micro-spec.rb
+++ lib/moon/modules/test/micro-spec.rb
@@ -4,6 +4,30 @@ module Moon
   ##
   # Basic Spec Framework
   module Test
+    def self.colorize(str, fg, bg = :default, mode = :default)
+      # swiped from colorize.rb
+      @color_codes ||= {
+        :black   => 0, :light_black    => 60,
+        :red     => 1, :light_red      => 61,
+        :green   => 2, :light_green    => 62,
+        :yellow  => 3, :light_yellow   => 63,
+        :blue    => 4, :light_blue     => 64,
+        :magenta => 5, :light_magenta  => 65,
+        :cyan    => 6, :light_cyan     => 66,
+        :white   => 7, :light_white    => 67,
+        :default => 9
+      }
+      @modes ={
+        :default   => 0, # Turn off all attributes
+        :bold      => 1, # Set bold mode
+        :underline => 4, # Set underline mode
+        :blink     => 5, # Set blink mode
+        :swap      => 7, # Exchange foreground and background colors
+        :hide      => 8  # Hide text (foreground color would be the same as background)
+      }
+      "\033[#{@modes[mode]};#{@color_codes[fg] + 30};#{@color_codes[bg] + 40}m#{str}\033[0m"
+    end
+
     module Spec
       def init_test_suite
         @tests = []
@@ -45,17 +69,21 @@ module Moon
         failures = []
         progress = []
         @tests.each do |a|
-          str, block = *a
+          test_name, block = *a
+          print format('%-0113s ', test_name)
           begin
             block.call
-            print '.' if realtime
+            #print '.' if realtime
+            print Test.colorize('PASSED', :light_green)
             progress << true
             passed += 1
           rescue => ex
-            print 'F' if realtime
+            #print 'F' if realtime
+            print Test.colorize('FAILED', :light_red)
             progress << false
-            failures << [str, ex.inspect.dup, ex.backtrace.dup]
+            failures << [test_name, ex.inspect.dup, ex.backtrace.dup]
           end
+          puts
         end
         time_now = Time.now
         unless realtime
