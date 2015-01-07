3c79872becd5bd3bbec647808afe93d02ad960ba
Corey Powell <mistdragon100@gmail.com>
2015-01-07 18:34:26 -0500

    data_bags: Added Table::Cursor

--- lib/moon/modules/data_bags/table.rb
+++ lib/moon/modules/data_bags/table.rb
@@ -39,6 +39,55 @@ module Moon #:nodoc:
       end
     end
 
+    # A Cursor is used to navigate a Table without having to specify the
+    # data position each time.
+    class Cursor
+      # @return [Moon::Table]
+      attr_reader :src
+      # @return [Moon::Vector2]
+      attr_accessor :position
+
+      # @param [Moon::Table] src
+      def initialize(src)
+        @position = Vector2.zero
+        @src = src
+      end
+
+      # Retrieve value at current +position+
+      # @overload get
+      # @overload get(x, y)
+      #   @param [Integer] x
+      #   @param [Integer] y
+      # @return [Integer]
+      def get(*args)
+        if args.size > 0
+          x, y = *args
+          src[position.x + x, position.y + y]
+        else
+          src[position.x, position.y]
+        end
+      end
+      alias :[] :get
+
+      # Set value at current +position+
+      # @overload put(value)
+      #   @param [Integer] value  Value to set
+      # @overload put(x, y, value)
+      #   @param [Integer] x
+      #   @param [Integer] y
+      #   @param [Integer] value  Value to set
+      def put(*args)
+        case args.size
+        when 1
+          src[position.x, position.y] = args.first
+        when 3
+          x, y, value = *args
+          src[position.x + x, position.y + y] = value
+        end
+      end
+      alias :[]= :put
+    end
+
     include Serializable
     include Serializable::PropertyHelper
 
