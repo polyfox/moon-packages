55db2680d695e1923d4fe6265cbcd0c5d0afe5d1
Corey Powell <mistdragon100@gmail.com>
2015-01-07 13:30:48 -0500

    std: Removed Array#get_args

--- lib/moon/modules/std/core_ext/array.rb
+++ lib/moon/modules/std/core_ext/array.rb
@@ -14,9 +14,4 @@ class Array
   def sort_by!(&block)
     sort! { |a, b| block.call(a) <=> block.call(b) }
   end unless method_defined?(:sort_by!)
-
-  # @see (Moon::ArgsHelper.get_args)
-  def get_args(format, *args, &block)
-    Moon::ArgsHelper.get_args(self, format, *args, &block)
-  end
 end
