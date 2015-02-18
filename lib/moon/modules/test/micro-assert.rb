f784fae2ba85fc317902da25030d9364f4bd5e09
Corey Powell <mistdragon100@gmail.com>
2015-02-18 14:58:06 -0500

    test: Updated assert and spec

--- lib/moon/modules/test/micro-assert.rb
+++ lib/moon/modules/test/micro-assert.rb
@@ -6,34 +6,75 @@ module Moon
   module Test
     module Assert
       class AssertError < StandardError
-        #
-      end
+        attr_accessor :test_stack
 
-      ##
-      # @return [Void]
-      private def init_assertions
-        @assertions = 0
-        @assertions_raised = 0
-      end
-
-      ##
-      # @param [String] msg
-      #   @optional
-      # @param [Exception] original_exception
-      #   @optional
-      private def raise_assert_error(msg = nil, original_exception = nil)
-        @assertions_raised += 1
-        if original_exception
-          fail AssertError, (msg || "assertion failed\n") + original_exception.inspect + "\n" + original_exception.backtrace("\n").indent(2)
-        else
-          fail AssertError, (msg || 'assertion failed')
+        def initialize(stack, message)
+          @test_stack = stack
+          super message
         end
       end
 
+      class << self
+        attr_accessor :fail_on_assert_error
+      end
+
+      self.fail_on_assert_error = true
+
+      attr_accessor :fail_on_assert_error
+      # whetehr or not to print out the err, only valid if fail_on_assert_error is false
+      attr_accessor :assertion_quiet
+      attr_reader :assertions
+      attr_reader :assertion_errors
+
+      ##
+      # @return [Void]
+      def init_assertions
+        @fail_on_assert_error = Assert.fail_on_assert_error
+        @assertions = 0
+        @assertion_errors = 0
+        @assertion_catcher = nil
+        @assertion_quiet = true
+      end
+      private :init_assertions
+
+      def catch_assertion_errors(&block)
+        @assertion_catcher = block
+      end
+
+      ##
+      # @param [String, nil] msg
+      # @param [Exception, nil] original_exception
+      def raise_assert_error(msg = nil, original_exception = nil)
+        if original_exception
+          message = (msg || "assertion failed\n") +
+                    original_exception.inspect + "\n" +
+                    original_exception.backtrace("\n").indent(2)
+        else
+          message = (msg || 'assertion failed.')
+        end
+        @assertion_errors += 1
+        begin
+          fail  # just to nab a backtrace
+        rescue RuntimeError => ex
+          # curry the error
+          err = AssertError.new(ex.backtrace.slice(1, ex.backtrace.size - 1),
+                                message)
+          fail err
+        end
+      rescue AssertError => err
+        @assertion_catcher.call err if @assertion_catcher
+        if @fail_on_assert_error
+          fail err
+        elsif !@assertion_quiet
+          puts err.inspect
+          puts err.backtrace.join("\n")
+        end
+      end
+      private :raise_assert_error
+
       ##
       # @param [Object] obj
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert(obj, msg = nil)
         @assertions += 1
         obj ? true : raise_assert_error(msg)
@@ -41,115 +82,114 @@ module Moon
 
       ##
       # @param [Object] obj
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_true(obj, msg = nil)
-        assert(!!obj, msg)
+        assert(!!obj, msg || "expected #{obj.inspect} to be true.")
       end
 
       ##
       # @param [Object] obj
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_false(obj, msg = nil)
-        assert_true(!obj, msg)
+        assert_true(!obj, msg || "expected #{obj.inspect} to be false.")
       end
 
       ##
       # @param [Object] a
       # @param [Object] b
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_equal(a, b, msg = nil)
-        assert_true(a == b, msg || "expected #{a} == #{b}")
+        assert_true(a == b, msg || "expected #{a.inspect} == #{b.inspect}.")
       end
 
       ##
       # @param [Object] a
       # @param [Object] b
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_not_equal(a, b, msg = nil)
-        assert_true(a != b, msg || "expected #{a} != #{b}")
+        assert_true(a != b, msg || "expected #{a.inspect} != #{b.inspect}.")
       end
 
       ##
       # @param [Object] a
       # @param [Object] b
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_same(a, b, msg = nil)
-        assert_true(a.equal?(b), msg || "expected #{a} to #equal? #{b}")
+        assert_true(a.equal?(b), msg || "expected #{a.inspect} to #equal? #{b.inspect}.")
       end
 
       ##
       # @param [Object] a
       # @param [Object] b
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_not_same(a, b, msg = nil)
-        assert_false(a.equal?(b), msg || "expected #{a} to not #equal? #{b}")
+        assert_false(a.equal?(b), msg || "expected #{a.inspect} to not #equal? #{b.inspect}.")
       end
 
       ##
       # @param [Object] a
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_nil(a, msg = nil)
         assert_equal(a, nil, msg)
       end
 
       ##
       # @param [Class<Exception>] klass
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       # @yield
       def assert_raise(klass, msg = nil)
         begin
           yield
         rescue Exception => e
-          assert_equal(e.class, klass, msg)
+          return assert_equal(e.class, klass, msg || "expected #{klass.inspect} to be raised")
         end
+        raise_assert_error("expected #{klass.inspect} to be raised, but nothing was raised.")
       end
 
       ##
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       # @yield
       def assert_not_raise(msg = nil)
         @assertions += 1
         begin
           yield
         rescue Exception => e
-          raise_assert_error(msg || 'expected no raise', e)
+          raise_assert_error(msg || 'expected nothing to be raised.', e)
         end
       end
 
       ##
       # @param [Class] klass
       # @param [Object] obj
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_kind_of(klass, obj, msg = nil)
-        assert_true(obj.kind_of?(klass), msg || "expected #{obj} to be kind_of? #{klass}")
+        assert_true(obj.kind_of?(klass),
+                    msg || "expected #{obj.inspect} to be kind_of? #{klass.inspect}.")
+      end
+
+      ##
+      # @param [Array<Class>] klasses
+      def assert_kind_of_any(klasses, obj, msg = nil)
+        assert_true(klasses.any? { |klass| obj.kind_of?(klass) },
+                    msg || "expected #{obj.inspect} to be kind_of any? #{klasses.inspect}")
       end
 
       ##
       # @param [Class] klass
       # @param [Object] obj
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_not_kind_of(klass, obj, msg = nil)
-        assert_false(obj.kind_of?(klass), msg || "expected #{obj} not to be kind_of? #{klass}")
+        assert_false(obj.kind_of?(klass),
+                     msg || "expected #{obj.inspect} not to be kind_of? #{klass.inspect}.")
       end
 
       ##
       # @param [Float] exp
       # @param [Float] act
-      # @param [String] msg
-      #   @optional
+      # @param [String, nil] msg
       def assert_float(exp, act, msg = nil)
-        assert_true(check_float(exp, act), msg || "expected #{exp} to equal #{act}")
+        assert_true(check_float(exp, act),
+                    msg || "expected #{exp.inspect} to equal #{act.inspect}.")
       end
 
       ##
@@ -172,7 +212,39 @@ module Moon
 
       # extended
       def assert_ary_size(expected, ary, msg = nil)
-        assert_equal(expected, ary.size, msg || "expected Array of size #{expected}")
+        assert_equal(expected, ary.size,
+                     msg || "expected Array of size #{expected.inspect}.")
+      end
+
+      # extended
+      def assert_ary_include(expected, ary, msg = nil)
+        assert_true(ary.include?(expected),
+                    msg || "expected Array of to include #{expected.inspect}.")
+      end
+
+      # @param [Symbol, String] constname
+      # @param [Module] parent
+      # @param [String, nil] msg
+      def assert_const_defined(constname, parent = nil, msg = nil)
+        parent ||= ::Object
+        assert_true(parent.const_defined?(constname),
+                    msg || "expected #{constname.inspect} to be defined under #{parent.inspect}.")
+      end
+
+      # @param [Symbol, String] methodname
+      # @param [Object] obj
+      # @param [String, nil] msg
+      def assert_respond_to(methodname, obj, msg = nil)
+        assert_true(obj.respond_to?(methodname),
+                    msg || "expected #{obj.inspect} to respond_to?(#{methodname.inspect}).")
+      end
+
+      # @param [Symbol, String] methodname
+      # @param [Object] obj
+      # @param [String, nil] msg
+      def assert_method_defined(methodname, obj, msg = nil)
+        assert_true(obj.method_defined?(methodname),
+                    msg || "expected #{obj.inspect} to define #{methodname.inspect}.")
       end
     end
     include Assert
--- lib/moon/modules/test/micro-spec.rb
+++ lib/moon/modules/test/micro-spec.rb
@@ -4,20 +4,21 @@ module Moon
   ##
   # Basic Spec Framework
   module Test
-    def self.colorize(str, fg, bg = :default, mode = :default)
+    module Colorize
       # swiped from colorize.rb
-      @color_codes ||= {
-        :black   => 0, :light_black    => 60,
-        :red     => 1, :light_red      => 61,
-        :green   => 2, :light_green    => 62,
-        :yellow  => 3, :light_yellow   => 63,
-        :blue    => 4, :light_blue     => 64,
-        :magenta => 5, :light_magenta  => 65,
-        :cyan    => 6, :light_cyan     => 66,
-        :white   => 7, :light_white    => 67,
-        :default => 9
+      CONSOLE_COLORS = {
+          :black   => 0, :light_black    => 60,
+          :red     => 1, :light_red      => 61,
+          :green   => 2, :light_green    => 62,
+          :yellow  => 3, :light_yellow   => 63,
+          :blue    => 4, :light_blue     => 64,
+          :magenta => 5, :light_magenta  => 65,
+          :cyan    => 6, :light_cyan     => 66,
+          :white   => 7, :light_white    => 67,
+          :default => 9
       }
-      @modes ={
+
+      CONSOLE_MODES = {
         :default   => 0, # Turn off all attributes
         :bold      => 1, # Set bold mode
         :underline => 4, # Set underline mode
@@ -25,18 +26,217 @@ module Moon
         :swap      => 7, # Exchange foreground and background colors
         :hide      => 8  # Hide text (foreground color would be the same as background)
       }
-      "\033[#{@modes[mode]};#{@color_codes[fg] + 30};#{@color_codes[bg] + 40}m#{str}\033[0m"
+
+      attr_accessor :colorize_enabled
+
+      def colorize_enabled?
+        @colorize_enabled.nil? ? Colorize.enabled : @colorize_enabled
+      end
+
+      def colorize(str, fg, bg = :default, mode = :default)
+        return str.dup unless colorize_enabled?
+        "\033[#{CONSOLE_MODES[mode]};#{CONSOLE_COLORS[fg] + 30};#{CONSOLE_COLORS[bg] + 40}m#{str}\033[0m"
+      end
+
+      class << self
+        attr_accessor :enabled
+      end
+
+      self.enabled = true
     end
 
-    module Spec
-      def init_test_suite
+    class Colorizer
+      include Colorize
+
+      def call(*args)
+        colorize(*args)
+      end
+    end
+
+    class NullOut
+      def write(*args, &block)
+      end
+
+      def print(*args, &block)
+      end
+
+      def puts(*args, &block)
+      end
+
+      def <<(*args, &block)
+      end
+    end
+
+    class Log
+      attr_reader :c
+      attr_accessor :io
+
+      def initialize(io = nil)
+        @c = Colorizer.new
+        @io = io || STDERR
+      end
+
+      def write(*args)
+        @io.write(*args)
+      end
+
+      def print(*args)
+        write(*args)
+      end
+
+      def puts(*args)
+        @io.puts(*args)
+      end
+
+      def prefixed_format(prefix, str, color = :default)
+        @c.call(prefix, :light_green) + str
+      end
+
+      def note(str)
+        puts prefixed_format('NOTE: ', str, :light_green)
+      end
+
+      def warn(str)
+        puts prefixed_format('WARN: ', str, :light_yellow)
+      end
+
+      def error(str)
+        puts prefixed_format('ERROR: ', str, :light_red)
+      end
+    end
+
+    class SpecSuite
+      class Stats
+        # @return [Moon::Test::Log]
+        attr_accessor :log
+        # @return [Float]
+        attr_accessor :time_diff
+        # @return [Array<Array<[String, Exception]>>]
+        attr_accessor :fail_stack
+        # @return [Array<Boolean>]
+        attr_accessor :result_stack
+        # @return [Integer]
+        attr_accessor :test_count
+        # @return [Integer]
+        attr_accessor :assertions
+        # @return [Integer]
+        attr_accessor :assertion_errors
+        # @return [Integer]
+        attr_accessor :passed
+
+        def initialize
+          @log = Log.new
+          @fail_stack = []
+          @result_stack = []
+          @time_diff = 0
+          @test_count = 0
+          @assertions = 0
+          @assertion_errors = 0
+          @passed = 0
+        end
+
+        def concat(stats)
+          @fail_stack.concat(stats.fail_stack)
+          @result_stack.concat(stats.result_stack)
+          @time_diff += stats.time_diff
+          @test_count += stats.test_count
+          @assertions += stats.assertions
+          @assertion_errors += stats.assertion_errors
+          @passed += stats.passed
+        end
+
+        def add_pass
+          @passed += 1
+          @result_stack << true
+        end
+
+        def add_failure(obj)
+          @result_stack << false
+          @fail_stack << obj
+        end
+
+        def display_result_stack
+          @log.puts
+          @result_stack.each do |status|
+            @log.print status ? '.' : 'F'
+          end
+        end
+
+        def display_backtraces
+          @log.puts
+          @fail_stack.each_with_index do |a, i|
+            str, ex = *a
+            @log.puts "##{i} #{str}"
+            @log.puts ex.inspect
+            @log.puts 'Backtrace: >'
+            ex.backtrace.each do |l|
+              @log.puts "  #{l}"
+            end
+
+            # suppress the test_stack
+            #if ex.respond_to?(:test_stack)
+            #  @log.puts 'Test Stack: >'
+            #  ex.test_stack.each do |l|
+            #    @log.puts "  #{l}"
+            #  end
+            #end
+            @log.puts
+          end
+        end
+
+        def display_test_count
+          @log.puts
+          @log.puts 'Tests: '
+          @log.puts '  Total: ' << @test_count.to_s
+          @log.puts '     OK: ' << @passed.to_s
+          @log.puts '   Fail: ' << (@test_count - @passed).to_s
+        end
+
+        def display_assertion_count
+          if @assertions > 0
+            @log.puts 'Assertions:'
+            @log.puts '  Total: ' << @assertions.to_s
+            @log.puts '     OK: ' << (@assertions - @assertion_errors).to_s
+            @log.puts '   Fail: ' << @assertion_errors.to_s
+          end
+        end
+
+        def display_time_diff
+          @log.puts 'Time: ' << (@time_diff).round(6).to_s[0, 8] << ' seconds'
+        end
+
+        def display
+          display_result_stack
+          display_backtraces
+          display_test_count
+          display_assertion_count
+          display_time_diff
+        end
+      end
+
+      include Colorize
+      include Moon::Test::Assert
+
+      attr_accessor :name
+      attr_accessor :log
+      attr_accessor :logger
+      attr_accessor :tests
+
+      def initialize(name = nil)
+        init_assertions
+        # spec log
+        @log = Log.new
+        # debug logger
+        @logger = NullOut.new
+        @name = name || "#{self.class.name}"
         @tests = []
         @test_stack = [@tests]
       end
 
       def describe(obj)
+        @logger.puts "#{name}.DESCRIBE: #{obj}"
         @test_stack << []
-        yield
+        yield self
         stack = @test_stack.pop
         stack.map! do |a|
           str, b = *a
@@ -47,8 +247,17 @@ module Moon
 
       alias :context :describe
 
+      def describe_top(&block)
+        describe(name, &block)
+      end
+
       def it(str, &block)
+        @logger.puts "#{name}.IT: #{str}"
         @test_stack[-1] << [str, block]
+      end
+
+      def given(str, &block)
+        it("given #{str}", &block)
       end
 
       def spec(str = '', &block)
@@ -63,59 +272,87 @@ module Moon
 
       def run_specs(options = {})
         with_assertions = options.fetch(:assertions, true)
-        realtime = options.fetch(:realtime_progress, true)
+        realtime = options.fetch(:realtime_result, true)
+        quiet = options.fetch(:quiet, false)
+
         time_then = Time.now
         passed = 0
-        failures = []
-        progress = []
+        stats = Stats.new
+        stats.log = @log
         @tests.each do |a|
           test_name, block = *a
-          print test_name
+          @log.print test_name
           if (n = 120 - test_name.size - 7) > 0
-            print '.' * n
+            @log.print '.' * n
           end
+
+          failure = false
+          catch_assertion_errors do |err|
+            failure = true
+            stats.add_failure [test_name, err]
+          end
+
           begin
-            block.call
-            #print '.' if realtime
-            print Test.colorize('PASSED', :light_green)
-            progress << true
-            passed += 1
-          rescue => ex
-            #print 'F' if realtime
-            print Test.colorize('FAILED', :light_red)
-            progress << false
-            failures << [test_name, ex.inspect.dup, ex.backtrace.dup]
+            instance_exec(&block)
+            if failure
+              @log.print colorize('FAILED', :light_red)
+            else
+              @log.print colorize('PASSED', :light_green)
+              stats.add_pass
+            end
+          rescue AssertError
+            # just ignore it
+          rescue Exception => ex
+            stats.add_failure [test_name, ex]
+            @log.puts colorize('KO!', :light_red)
+            @log.puts ex.inspect
+            @log.puts ex.backtrace.join("\n")
           end
-          puts
+          @log.puts
         end
+
         time_now = Time.now
-        unless realtime
-          puts
-          progress.each do |status|
-            print status ? '.' : 'F'
-          end
-        end
-        puts
-        failures.each_with_index do |a, i|
-          str, inspect, backtrace = *a
-          puts "##{i} #{str}"
-          puts inspect
-          puts backtrace.join("\n").indent(2)
-        end
-        puts
-        puts 'Tests: '
-        puts '  Total: ' << @tests.size.to_s
-        puts '     OK: ' << passed.to_s
-        puts '   Fail: ' << (@tests.size - passed).to_s
-        if with_assertions
-          puts 'Assertions:'
-          puts '  Total: ' << @assertions.to_s
-          puts '     OK: ' << (@assertions - @assertions_raised).to_s
-          puts '   Fail: ' << @assertions_raised.to_s
-        end
-        puts 'Time: ' << (time_now - time_then).round(6).to_s[0, 8] << ' seconds'
+        stats.time_diff = (time_now - time_then)
+        stats.test_count = @tests.size
+        stats.assertions = @assertions
+        stats.assertion_errors = @assertion_errors
+        stats.display unless quiet
+        stats
       end
     end
+
+    module Spec
+      attr_accessor :spec_suite
+
+      def init_test_suite
+        @spec_suite = SpecSuite.new
+      end
+
+      def describe(*a, &b)
+        @spec_suite.describe(*a, &b)
+      end
+
+      def context(*a, &b)
+        @spec_suite.context(*a, &b)
+      end
+
+      def it(*a, &b)
+        @spec_suite.it(*a, &b)
+      end
+
+      def spec(*a, &b)
+        @spec_suite.spec(*a, &b)
+      end
+
+      def spec_bm(*a, &b)
+        @spec_suite.spec_bm(*a, &b)
+      end
+
+      def run_specs(*a, &b)
+        @spec_suite.run_specs(*a, &b)
+      end
+    end
+
     include Spec
   end
 end
