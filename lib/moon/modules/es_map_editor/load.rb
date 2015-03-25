36143290612e30bd3e473c7c1d9946324e66a7fb
Corey Powell <mistdragon100@gmail.com>
2015-03-25 13:39:13 -0500

    ed_map_editor: Moved require models/* to models.rb

--- lib/moon/modules/es_map_editor/load.rb
+++ lib/moon/modules/es_map_editor/load.rb
@@ -1,11 +1,7 @@
 require 'es_map_editor/camera_cursor'
 require 'es_map_editor/map_cursor'
 
-require 'es_map_editor/models/tileset'
-require 'es_map_editor/models/editor_chunk'
-require 'es_map_editor/models/editor_map'
-require 'es_map_editor/models/editor_tile_palette'
-require 'es_map_editor/models/map_editor_model'
+require 'es_map_editor/models'
 
 require 'es_map_editor/selection_tile_rect'
 require 'es_map_editor/views/map_view'
---
+++ lib/moon/modules/es_map_editor/models.rb
@@ -0,0 +1,5 @@
+require 'es_map_editor/models/tileset'
+require 'es_map_editor/models/editor_chunk'
+require 'es_map_editor/models/editor_map'
+require 'es_map_editor/models/editor_tile_palette'
+require 'es_map_editor/models/map_editor_model'
