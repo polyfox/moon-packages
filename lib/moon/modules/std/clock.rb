f598158c5113236cc1236cf9abbdd3ee1f272027
Corey Powell <mistdragon100@gmail.com>
2015-03-25 13:31:36 -0500

    std: Clock is builtin now

--- lib/moon/modules/std/clock.rb
+++ lib/moon/modules/std/clock.rb
@@ -1,24 +1,5 @@
-module Moon
+module Moon #:nodoc:
   class Clock
-    attr_reader :start_time
-
-    def initialize
-      @start_time = current_time
-    end
-
-    def current_time
-      Time.now.to_f
-    end
-
-    def elapsed
-      current_time - @start_time
-    end
-
-    def restart
-      now = current_time
-      elasped = now - @start_time
-      @start_time = now
-      return elasped
-    end
+    #
   end
 end
