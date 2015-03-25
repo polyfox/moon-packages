e418ff19ec6ce59ad554dd0ba295569bfdec68bb
Corey Powell <mistdragon100@gmail.com>
2015-03-25 13:32:49 -0500

    scheduler/jobs: Timeouts now mark if they've been triggered

--- lib/moon/modules/scheduler/jobs/timeout.rb
+++ lib/moon/modules/scheduler/jobs/timeout.rb
@@ -4,16 +4,21 @@ module Moon #:nodoc:
       # Timeouts are one off tasks which will execute after their time has
       # expired.
       class Timeout < TimeBase
+        # Whether this timeout has already called its trigger method.
+        # @return [Boolean]
+        attr_reader :triggered
+
         # Has this timeout reached its end?
         #
         # @return [Boolean]
         def done?
-          @time <= 0
+          @time <= 0 && @triggered
         end
 
         # When time reaches 0 or less
         def on_timeout
           trigger
+          @triggered = true
           deactivate
         end
       end
