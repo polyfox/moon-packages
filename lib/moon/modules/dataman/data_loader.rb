66f07bd34577bb38b79e5d635efca140fd1da0ac
Corey Powell <mistdragon100@gmail.com>
2015-03-27 14:21:42 -0500

    dataman: Refactored

--- lib/moon/modules/dataman/data_loader.rb
+++ lib/moon/modules/dataman/data_loader.rb
@@ -1,16 +1,35 @@
-##
-# DataLoader assumes that all data will be serialized as YAML
-module DataLoader
-  def self.string(string)
-    YAML.load(string)
-  end
+module Moon #:nodoc:
+  # DataLoader is a helper module for loading data from the filesystem.
+  # DataLoader assumes that all data is serialized as YAML with the file
+  # extension .yml.
+  module DataLoader
+    # Rootpath of the data, defauled to "data"
+    # @return [String]
+    def self.rootpath
+      "data"
+    end
 
-  def self.raw_file(filename)
-    File.read("data/#{filename}.yml")
-  end
+    # Loads data from a String
+    #
+    # @return [String]
+    def self.string(string)
+      YAML.load(string)
+    end
 
-  def self.file(filename)
-    STDERR.puts "[#{self}] .file(#{filename})"
-    string(raw_file(filename))
+    # Loads data from a file as is.
+    #
+    # @param [String] filename
+    # @return [String]
+    def self.raw_file(filename)
+      File.read("#{rootpath}/#{filename}.yml")
+    end
+
+    # Loads data from a file and loads it using YAML
+    #
+    # @param [String] filename
+    def self.file(filename)
+      STDERR.puts "[#{self}] .file(#{filename})"
+      string raw_file(filename)
+    end
   end
 end
--- lib/moon/modules/dataman/data_serializer.rb
+++ lib/moon/modules/dataman/data_serializer.rb
@@ -1,71 +1,72 @@
-module DataSerializer
-  def self.resolve_references(data, depth = 0)
-    case data
-    when Array
-      data.map do |obj|
-        resolve_references(obj, depth + 1)
-      end
-    when Hash
-      if refname = data['&ref']
-        DataLoader.file(refname)
-      else
-        data.each_with_object({}) do |a, r|
-          k, v = *a
-          r[k] = resolve_references(v, depth + 1)
+module Moon #:nodoc:
+  module DataSerializer
+    class Resolver
+      def resolve(data, depth)
+        case data
+        when Array
+          data.map do |obj|
+            resolve(obj, depth + 1)
+          end
+        when Hash
+          if refname = data['&ref']
+            DataLoader.file(refname)
+          else
+            data.each_with_object({}) do |a, r|
+              k, v = *a
+              r[k] = resolve(v, depth + 1)
+            end
+          end
+        else
+          data
         end
       end
-    else
-      data
+
+      def self.resolve(data, depth = 0)
+        new.resolve(data, depth)
+      end
     end
-  end
 
-  def self.load_obj_from_classname(classname, data, depth = 0)
-    Object.const_get(classname).load(data, depth + 1)
-  end
-
-  def self.load_obj_hash(data, depth = 0)
-    result = {}
-    data.each do |key, value|
-      result[key] = load_obj(value, depth + 1)
+    def self.load_obj_from_classname(classname, data, depth = 0)
+      Object.const_get(classname).load(data, depth + 1)
     end
-    result
-  end
 
-  def self.load_obj(data, depth = 0)
-    if data.is_a?(Hash)
-      if data.key?('&class')
-        load_obj_from_classname(data['&class'], data, depth)
+    def self.load_obj_hash(data, depth = 0)
+      result = {}
+      data.each do |key, value|
+        result[key] = load_obj(value, depth + 1)
+      end
+      result
+    end
+
+    def self.load_obj(data, depth = 0)
+      if data.is_a?(Hash)
+        if data.key?('&class')
+          load_obj_from_classname(data['&class'], data, depth)
+        else
+          load_obj_hash(data, depth)
+        end
+      elsif data.is_a?(Array)
+        data.map do |value|
+          load_obj(value)
+        end
       else
-        load_obj_hash(data, depth)
+        data
       end
-    elsif data.is_a?(Array)
-      data.map do |value|
-        load_obj(value)
-      end
-    else
-      data
     end
-  end
 
-  def self.load(data, depth = 0)
-    load_obj(resolve_references(data, depth + 1))
-  end
+    def self.load(data, depth = 0)
+      load_obj Resolver.resolve(data, depth + 1)
+    end
 
-  def self.load_file(filename, depth = 0)
-    data = DataLoader.raw_file(filename)
-    load(DataLoader.string(data), depth + 1)
-  end
+    def self.load_file(filename, depth = 0)
+      data = DataLoader.raw_file(filename)
+      load DataLoader.string(data), depth + 1
+    end
 
-  ##
-  # @todo
-  # @param [Object] obj
-  # @param [Integer] depth
-  def self.dump(obj, depth = 0)
-    # TODO
-  end
-
-  class << self
-    private :resolve_references
-    private :load_obj
+    class << self
+      private :load_obj_from_classname
+      private :load_obj_hash
+      private :load_obj
+    end
   end
 end
