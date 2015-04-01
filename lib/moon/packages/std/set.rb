36b7c80cf99031b6e1bef1bf53694427f6c6139f
Corey Powell <mistdragon100@gmail.com>
2015-04-01 12:01:49 -0500

    Update Set implementation

--- lib/moon/packages/std/set.rb
+++ lib/moon/packages/std/set.rb
@@ -1,7 +1,44 @@
 # Basic implementation of Set.
 class Set
-  def initialize
+  include Enumerable
+
+  # @param [#each] data
+  def initialize(enum = nil, &block)
     @data = {}
+    if block_given?
+      enum.times { |i| add block.call(i) }
+    else
+      concat enum if enum
+    end
+  end
+
+  # Returns an Array from the Set elements.
+  #
+  # @return [Array<Object>]
+  def to_a
+    @data.keys.to_a
+  end
+
+  # Returns the size of the Set.
+  #
+  # @return [Integer]
+  def size
+    @data.size
+  end
+
+  # Determines if the Set is empty?
+  #
+  # @return [Boolean]
+  def empty?
+    @data.empty?
+  end
+
+  # Determins if the Set includes a object
+  #
+  # @param [Object] obj
+  # @return [Boolean]
+  def include?(obj)
+    @data.key?(obj)
   end
 
   # Yields each object in the Set.
@@ -12,13 +49,40 @@ class Set
     @data.keys.each(&block)
   end
 
-  # Adds an object to the Set.
-  # @param [Object] value
-  def push(value)
-    @data[value] = true
+  # Determines if the Set matches the given object
+  #
+  # @param [Object] obj
+  # @return [Boolean]
+  def ==(other)
+    if other.equal?(self)
+      true
+    elsif other.is_a?(Set) || other.is_a?(Array)
+      other.size == size && other.all? { |e| include?(e) }
+    else
+      false
+    end
+  end
+
+  # Clear the Set.
+  #
+  # @return [self]
+  def clear
+    @data.clear
     self
   end
-  alias :<< :push
+
+  # Adds an object to the Set.
+  #
+  # @param [Object] value
+  def add(*values)
+    values.each do |e|
+      @data[e] = true
+    end
+    self
+  end
+  alias :push :add
+  alias :<< :add
+  alias :unshift :add
 
   # Removes the object from the set and returns it.
   #
@@ -31,16 +95,18 @@ class Set
 
   # Appends the +other+ to the current set
   #
-  # @param [Set, Array, #each] other
+  # @param [#each] other
   # @return [self]
   def concat(other)
     other.each do |obj|
-      push obj
+      add obj
     end
     self
   end
 
   # Returns a random object from the Set.
+  #
+  # @return [Object]
   def sample
     @data.keys.sample
   end
