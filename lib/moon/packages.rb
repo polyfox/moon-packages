8056ecf9b0400c96f67111427a36da12f286f110
Corey Powell <mistdragon100@gmail.com>
2015-04-07 14:11:00 -0500

    moon: packages bootstrap $LOAD_PATH for regular ruby

--- lib/moon/packages.rb
+++ lib/moon/packages.rb
@@ -1,10 +1,15 @@
 MOON_PACKAGE_ROOT = File.join(File.dirname(__FILE__), 'packages')
 
 module Moon
+  def self.bootstrap_packagedir
+    $LOAD_PATH << MOON_PACKAGE_ROOT unless $LOAD_PATH.include?(MOON_PACKAGE_ROOT)
+  end
+
   def self.require_package(basename)
     require File.join(MOON_PACKAGE_ROOT, basename)
   end
 end
+
 ##
 # :nodoc:
 module Kernel
@@ -16,3 +21,5 @@ module Kernel
     require_package basename
   end
 end
+
+Moon.bootstrap_packagedir
