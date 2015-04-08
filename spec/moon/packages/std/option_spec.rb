8ffd9522ee6f92b0522212d222854a0d72567aeb
Corey Powell <mistdragon100@gmail.com>
2015-04-07 23:35:34 -0500

    Updated option spec with described_class

--- spec/moon/packages/std/option_spec.rb
+++ spec/moon/packages/std/option_spec.rb
@@ -4,7 +4,7 @@ describe Moon::Option do
   context '#initialize' do
     it 'should initialize with a value' do
       my_value = 'Hello World :3'
-      opt = Moon::Option.new(my_value)
+      opt = described_class.new(my_value)
       expect(opt.value).to eq(my_value)
     end
   end
@@ -12,38 +12,38 @@ describe Moon::Option do
   context '#map' do
     it 'should eval and replace if it has a non-blank value' do
       my_value = 'Hello World :3'
-      opt = Moon::Option.new(my_value)
+      opt = described_class.new(my_value)
       opt.map { |v| v.reverse }
       expect(opt.value).to eq(my_value.reverse)
     end
 
     it 'should not replace, if it has a blank value' do
-      opt = Moon::Option.new(nil)
+      opt = described_class.new(nil)
       opt.map { |v| 'Egg' }
-      expect(opt.value).to eq(nil)
+      expect(opt.value).to be_nil
     end
   end
 
   context '#blank?' do
     it 'should report blank if the value is nil' do
-      opt = Moon::Option.new(nil)
-      expect(opt.blank?).to eq(true)
+      opt = described_class.new(nil)
+      expect(opt).to be_blank
     end
 
     it 'should report non-blank if the value is anything except nil' do
-      opt = Moon::Option.new(false)
+      opt = described_class.new(false)
       expect(opt.blank?).to eq(false)
     end
   end
 
   context '#presence' do
     it 'should return nil if blank' do
-      opt = Moon::Option.new(nil)
-      expect(opt.presence).to eq(nil)
+      opt = described_class.new(nil)
+      expect(opt.presence).to be_nil
     end
 
     it 'should return value if non-blank' do
-      opt = Moon::Option.new(false)
+      opt = described_class.new(false)
       expect(opt.presence).to eq(false)
     end
   end
