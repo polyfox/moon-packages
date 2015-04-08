79a2360ccba8d7d63632050d3b4023518739c62c
Corey Powell <mistdragon100@gmail.com>
2015-04-07 20:07:12 -0500

    std: Moved some top level classes into the Moon module

--- lib/moon/packages/std/palette_parser.rb
+++ lib/moon/packages/std/palette_parser.rb
@@ -1,80 +1,82 @@
-module PaletteParser
-  class FormatError < StandardError
-  end
-
-  def self.load_palette_hex(colors, bits, format_rgx, format_str)
-    bpc = (bits / 3).to_i # bits per channel
-    bitmask = 2 ** bpc - 1
-    colors.each_with_object({}) do |a, hash|
-      key, hex = *a
-      matchdata = hex.match(format_rgx)
-      raise FormatError, "invalid hex format (#{hex}) expected (#{format_str})" unless matchdata
-      value = matchdata[0].to_i(16)
-      r = (value >> (bpc * 2)) & bitmask
-      g = (value >> bpc) & bitmask
-      b = value & bitmask
-      hash[key] = Moon::Vector4.new(r/bitmask.to_f, g/bitmask.to_f, b/bitmask.to_f, 1.0)
+module Moon
+  module PaletteParser
+    class FormatError < StandardError
     end
-  end
 
-  def self.load_palette_hex_rgb(colors)
-    load_palette_hex(colors, 12, /[0-9a-fA-F]{3}/, "RGB")
-  end
-
-  def self.load_palette_hex_rrggbb(colors)
-    load_palette_hex(colors, 24, /[0-9a-fA-F]{6}/, "RRGGBB")
-  end
-
-  def self.load_palette_array_rgb(colors)
-    colors.each_with_object({}) do |a, hash|
-      key, ary = *a
-      r, g, b = *ary
-      hash[key] = Moon::Vector4.new(r/255.0, g/255.0, b/255.0, 1.0)
+    def self.load_palette_hex(colors, bits, format_rgx, format_str)
+      bpc = (bits / 3).to_i # bits per channel
+      bitmask = 2 ** bpc - 1
+      colors.each_with_object({}) do |a, hash|
+        key, hex = *a
+        matchdata = hex.match(format_rgx)
+        raise FormatError, "invalid hex format (#{hex}) expected (#{format_str})" unless matchdata
+        value = matchdata[0].to_i(16)
+        r = (value >> (bpc * 2)) & bitmask
+        g = (value >> bpc) & bitmask
+        b = value & bitmask
+        hash[key] = Moon::Vector4.new(r/bitmask.to_f, g/bitmask.to_f, b/bitmask.to_f, 1.0)
+      end
     end
-  end
 
-  def self.load_palette_array_rgba(colors)
-    colors.each_with_object({}) do |a, hash|
-      key, ary = *a
-      r, g, b, a = *ary
-      hash[key] = Moon::Vector4.new(r/255.0,g/255.0,b/255.0,a/255.0)
+    def self.load_palette_hex_rgb(colors)
+      load_palette_hex(colors, 12, /[0-9a-fA-F]{3}/, "RGB")
     end
-  end
 
-  def self.load_palette_float_array_rgb(colors)
-    colors.each_with_object({}) do |a, hash|
-      key, ary = *a
-      r, g, b = *ary
-      hash[key] = Moon::Vector4.new(r,g,b,1.0)
+    def self.load_palette_hex_rrggbb(colors)
+      load_palette_hex(colors, 24, /[0-9a-fA-F]{6}/, "RRGGBB")
     end
-  end
 
-  def self.load_palette_float_array_rgba(colors)
-    colors.each_with_object({}) do |a, hash|
-      key, ary = *a
-      r, g, b, a = *ary
-      hash[key] = Moon::Vector4.new(r,g,b,a)
+    def self.load_palette_array_rgb(colors)
+      colors.each_with_object({}) do |a, hash|
+        key, ary = *a
+        r, g, b = *ary
+        hash[key] = Moon::Vector4.new(r/255.0, g/255.0, b/255.0, 1.0)
+      end
     end
-  end
 
-  def self.load_palette(data)
-    colors = data.fetch("colors")
-    format = data.fetch("format")
-    case format
-    when "hex.RGB"
-      load_palette_hex_rgb colors
-    when "hex.RRGGBB"
-      load_palette_hex_rrggbb colors
-    when "[rgb]"
-      load_palette_array_rgb colors
-    when "[rgba]"
-      load_palette_array_rgba colors
-    when "float[rgb]"
-      load_palette_float_array_rgb colors
-    when "float[rgba]"
-      load_palette_float_array_rgba colors
-    else
-      raise FormatError, "unsupported palette format #{format}"
+    def self.load_palette_array_rgba(colors)
+      colors.each_with_object({}) do |a, hash|
+        key, ary = *a
+        r, g, b, a = *ary
+        hash[key] = Moon::Vector4.new(r/255.0,g/255.0,b/255.0,a/255.0)
+      end
+    end
+
+    def self.load_palette_float_array_rgb(colors)
+      colors.each_with_object({}) do |a, hash|
+        key, ary = *a
+        r, g, b = *ary
+        hash[key] = Moon::Vector4.new(r,g,b,1.0)
+      end
+    end
+
+    def self.load_palette_float_array_rgba(colors)
+      colors.each_with_object({}) do |a, hash|
+        key, ary = *a
+        r, g, b, a = *ary
+        hash[key] = Moon::Vector4.new(r,g,b,a)
+      end
+    end
+
+    def self.load_palette(data)
+      colors = data.fetch("colors")
+      format = data.fetch("format")
+      case format
+      when "hex.RGB"
+        load_palette_hex_rgb colors
+      when "hex.RRGGBB"
+        load_palette_hex_rrggbb colors
+      when "[rgb]"
+        load_palette_array_rgb colors
+      when "[rgba]"
+        load_palette_array_rgba colors
+      when "float[rgb]"
+        load_palette_float_array_rgb colors
+      when "float[rgba]"
+        load_palette_float_array_rgba colors
+      else
+        raise FormatError, "unsupported palette format #{format}"
+      end
     end
   end
 end
--- lib/moon/packages/std/set.rb
+++ lib/moon/packages/std/set.rb
@@ -1,127 +1,129 @@
-# Basic implementation of Set.
-class Set
-  include Enumerable
+module Moon
+  # Basic implementation of Set.
+  class Set
+    include Enumerable
 
-  # @param [#each] enum
-  def initialize(enum = nil, &block)
-    @data = {}
-    if block_given?
-      enum.times { |i| add block.call(i) }
-    else
-      concat enum if enum
+    # @param [#each] enum
+    def initialize(enum = nil, &block)
+      @data = {}
+      if block_given?
+        enum.times { |i| add block.call(i) }
+      else
+        concat enum if enum
+      end
     end
-  end
 
-  # Returns an Array from the Set elements.
-  #
-  # @return [Array<Object>]
-  def to_a
-    @data.keys.to_a
-  end
-
-  # Returns the size of the Set.
-  #
-  # @return [Integer]
-  def size
-    @data.size
-  end
-
-  # Determines if the Set is empty?
-  #
-  # @return [Boolean]
-  def empty?
-    @data.empty?
-  end
-
-  # Determins if the Set includes a object
-  #
-  # @param [Object] obj
-  # @return [Boolean]
-  def include?(obj)
-    @data.key?(obj)
-  end
-
-  # Yields each object in the Set.
-  #
-  # @yieldparam [Object] obj
-  def each(&block)
-    return to_enum :each unless block_given?
-    @data.keys.each(&block)
-  end
-
-  # Determines if the Set matches the given object
-  #
-  # @param [Object] other
-  # @return [Boolean]
-  def ==(other)
-    if other.equal?(self)
-      true
-    elsif other.is_a?(Set) || other.is_a?(Array)
-      other.size == size && other.all? { |e| include?(e) }
-    else
-      false
+    # Returns an Array from the Set elements.
+    #
+    # @return [Array<Object>]
+    def to_a
+      @data.keys.to_a
     end
-  end
 
-  # Clear the Set.
-  #
-  # @return [self]
-  def clear
-    @data.clear
-    self
-  end
-
-  # Adds an object to the Set.
-  #
-  # @param [Object] values
-  def add(*values)
-    values.each do |e|
-      @data[e] = true
+    # Returns the size of the Set.
+    #
+    # @return [Integer]
+    def size
+      @data.size
     end
-    self
-  end
-  alias :push :add
-  alias :<< :add
-  alias :unshift :add
 
-  # Removes the object from the set and returns it.
-  #
-  # @param [Object] value
-  # @return [Object]
-  def delete(value)
-    @data.delete value
-    value
-  end
-
-  # Appends the +other+ to the current set
-  #
-  # @param [#each] other
-  # @return [self]
-  def concat(other)
-    other.each do |obj|
-      add obj
+    # Determines if the Set is empty?
+    #
+    # @return [Boolean]
+    def empty?
+      @data.empty?
     end
-    self
-  end
 
-  # Returns a random object from the Set.
-  #
-  # @return [Object]
-  def sample
-    @data.keys.sample
-  end
+    # Determins if the Set includes a object
+    #
+    # @param [Object] obj
+    # @return [Boolean]
+    def include?(obj)
+      @data.key?(obj)
+    end
 
-  # Pops an object from the set, may be random.
-  #
-  # @return [Object]
-  def pop
-    delete @data.keys.pop
-  end
+    # Yields each object in the Set.
+    #
+    # @yieldparam [Object] obj
+    def each(&block)
+      return to_enum :each unless block_given?
+      @data.keys.each(&block)
+    end
 
-  # Shifts an object from the set, may be random.
-  #
-  # @return [Object]
-  def shift
-    delete @data.keys.shift
+    # Determines if the Set matches the given object
+    #
+    # @param [Object] other
+    # @return [Boolean]
+    def ==(other)
+      if other.equal?(self)
+        true
+      elsif other.is_a?(Set) || other.is_a?(Array)
+        other.size == size && other.all? { |e| include?(e) }
+      else
+        false
+      end
+    end
+
+    # Clear the Set.
+    #
+    # @return [self]
+    def clear
+      @data.clear
+      self
+    end
+
+    # Adds an object to the Set.
+    #
+    # @param [Object] values
+    def add(*values)
+      values.each do |e|
+        @data[e] = true
+      end
+      self
+    end
+    alias :push :add
+    alias :<< :add
+    alias :unshift :add
+
+    # Removes the object from the set and returns it.
+    #
+    # @param [Object] value
+    # @return [Object]
+    def delete(value)
+      @data.delete value
+      value
+    end
+
+    # Appends the +other+ to the current set
+    #
+    # @param [#each] other
+    # @return [self]
+    def concat(other)
+      other.each do |obj|
+        add obj
+      end
+      self
+    end
+
+    # Returns a random object from the Set.
+    #
+    # @return [Object]
+    def sample
+      @data.keys.sample
+    end
+
+    # Pops an object from the set, may be random.
+    #
+    # @return [Object]
+    def pop
+      delete @data.keys.pop
+    end
+
+    # Shifts an object from the set, may be random.
+    #
+    # @return [Object]
+    def shift
+      delete @data.keys.shift
+    end
   end
 end
