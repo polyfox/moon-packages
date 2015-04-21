86785bb9df8dfce7ad331bdec4a9979c6a37cf45
Corey Powell <mistdragon100@gmail.com>
2015-04-21 15:04:33 -0500

    DataModel has been moved to moon-data_model

--- lib/moon/packages/data_model/load.rb
+++
@@ -1,9 +0,0 @@
-require 'data_model/type_validators'
-require 'data_model/validators'
-require 'data_model/field'
-require 'data_model/fields'
-require 'data_model/access/square_bracket'
-require 'data_model/model'
-require 'data_model/metal'
-require 'data_model/base'
-Moon::DataModel::Field.type_validator = Moon::DataModel::TypeValidators::Soft
--- lib/moon/packages/data_model/type_validators.rb
+++
@@ -1,4 +0,0 @@
-require 'data_model/type_validators/base'
-require 'data_model/type_validators/null'
-require 'data_model/type_validators/soft'
-require 'data_model/type_validators/verbose'
