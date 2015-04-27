a7db19abc6c81983617c3c930a2a7ee8b7e48785
Corey Powell <mistdragon100@gmail.com>
2015-04-27 08:41:58 -0500

    TMX has been moved to moon-tmx

--- lib/moon/packages/tmx/load.rb
+++
@@ -1,5 +0,0 @@
-require 'tmx/models/object'
-require 'tmx/models/tileset'
-require 'tmx/models/layer'
-require 'tmx/models/map'
-require 'tmx/tmx'
--- spec/moon/packages/tmx/tmx_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'data_model/load'
-require 'tmx/load'
-
-describe TMX do
-end
