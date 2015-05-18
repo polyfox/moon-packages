8fc918d57615f9115c6f00bfcbb3195360b852e0
Corey Powell <mistdragon100@gmail.com>
2015-05-18 11:54:57 -0500

    Updated AABBCC spec

--- spec/moon/packages/std/aabbcc_spec.rb
+++ spec/moon/packages/std/aabbcc_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 require 'std/aabbcc'
 
 describe Moon::AABBCC do
+  context '#initialize' do
+    it 'initializes an aabbcc' do
+      aabbcc = described_class.new(0, 4)
+    end
+  end
+
+  context '#intersect?' do
+    it 'checks if the aabb intersect with another' do
+      aabbcc1 = described_class.new(0, 4)
+      aabbcc2 = described_class.new(0, 4)
+      aabbcc1.intersect?(aabbcc2)
+    end
+  end
 end
