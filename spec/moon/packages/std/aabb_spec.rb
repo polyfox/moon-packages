6a1b7a315d3c5038145d178e1ab8454f660e26be
Corey Powell <mistdragon100@gmail.com>
2015-05-18 00:27:37 -0500

    Updated AABB spec

--- spec/moon/packages/std/aabb_spec.rb
+++ spec/moon/packages/std/aabb_spec.rb
@@ -2,4 +2,17 @@ require 'spec_helper'
 require 'std/aabb'
 
 describe Moon::AABB do
+  context '#initialize' do
+    it 'initializes an aabb' do
+      aabb = described_class.new(0, 4)
+    end
+  end
+
+  context '#intersect?' do
+    it 'checks if the aabb intersect with another' do
+      aabb1 = described_class.new(0, 4)
+      aabb2 = described_class.new(0, 4)
+      aabb1.intersect?(aabb2)
+    end
+  end
 end
