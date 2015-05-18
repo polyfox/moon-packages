ceb12f5122f9704e58dfc0627c4f60fd3ab68ecc
Corey Powell <mistdragon100@gmail.com>
2015-05-18 11:55:16 -0500

    Added #fixture_pathname spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@ require 'codeclimate-test-reporter'
 
 require 'simplecov'
 
+def fixture_pathname(*args)
+  File.join(File.dirname(__FILE__), *args)
+end
 
 CodeClimate::TestReporter.start
 SimpleCov.start
