a085b4eaa7d71378870c8d466ab475dd9c016ef8
Corey Powell <mistdragon100@gmail.com>
2015-05-18 11:43:49 -0500

    Fixed some typos, restored #each_listener, fixed spec
    #each_listener is required by the spec to check for active listeners
    of a group, it is also needed by the debug package for checking
    all registered listeners for an Eventable.
    
    ┐(￣ヮ￣)┌

--- lib/moon/packages/std/mixins/eventable.rb
+++ lib/moon/packages/std/mixins/eventable.rb
@@ -1,5 +1,3 @@
-require 'std/core_ext/array'
-require 'std/core_ext/symbolic'
 require 'std/event'
 
 module Moon
@@ -14,30 +12,46 @@ module Moon
       !@listeners.empty?
     end
 
+    # Yields each listener of given type, defaults to :any
+    #
+    # @param [Symbol] type
+    # @yieldparam [Symbol] type
+    # @yieldparam [#call] block
+    def each_listener(*types, &block)
+      return to_enum(:each_listener, *types) unless block_given?
+      if types.empty?
+        @listeners.each { |key, ary| ary.each { |cb| block.call(key, cb) } }
+      else
+        types.each do |type|
+          @listeners[type].each { |cb| block.call(type, cb) }
+        end
+      end
+    end
+
     # @!group Registration
 
     # Removes a listener.
     #
-    # @param [Boolean] true
+    # @param [Array<Object>] listeners
     def off(*listeners)
-      @listeners.each { |_, ary| ary.delete(listener) }
-      true
+      @listeners.each do |_, ary|
+        listeners.each { |cb| ary.delete(cb) }
+      end
     end
 
     # Adds a new event listener.
     #
-    # @param [Symbol, Class<Event>] events  events to listen for
-    # @param [Proc] block The block we want to execute when we catch the event.
-    # @return [Boolean]
+    # @param [Symbol] events  events to listen for
+    # @param [Proc] block  The block we want to execute when we catch the event.
+    # @return [#call] the block passed in
     def on(*events, &block)
-      events.each { |event| @listener[event] << block }
-      @listener[:any] << block
-      true
+      events.each { |event| @listeners[event.to_sym] << block }
+      block
     end
 
     # Shortcut for `on :typing, &block`
     #
-    # @return [Listener]
+    # @return [Proc] block passed in
     def typing(&block)
       on(:typing, &block)
     end
@@ -60,8 +74,8 @@ module Moon
 
       return unless allow_event?(event)
 
-      @listeners[:any].each { |listener| listener.call event }
-      @listeners[event.type].each { |listener| listener.call event }
+      each_listener(:any) { |_, listener| listener.call event }
+      each_listener(event.type) { |_, listener| listener.call event }
     end
   end
 end
