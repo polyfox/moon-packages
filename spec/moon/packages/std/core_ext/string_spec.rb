c1c8735b071b73d0f760512888f744e8898522de
Corey Powell <mistdragon100@gmail.com>
2015-05-18 00:12:39 -0500

    Updated String spec

--- spec/moon/packages/std/core_ext/string_spec.rb
+++ spec/moon/packages/std/core_ext/string_spec.rb
@@ -3,4 +3,13 @@ require 'std/core_ext/object'
 require 'std/core_ext/string'
 
 describe String do
+  context '#indent' do
+    it 'indents a string' do
+      expect('Hello'.indent(2)).to eq('  Hello')
+    end
+
+    it 'indents a string with multiple lines' do
+      expect("Hello\nWorld\nHow\nAre\nYou".indent(2)).to eq("  Hello\n  World\n  How\n  Are\n  You")
+    end
+  end
 end
