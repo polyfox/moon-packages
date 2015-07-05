da1414e3b7e24e507c310de6505ea31a979374df
Corey Powell <mistdragon100@gmail.com>
2015-07-05 13:06:41 -0500

    render_primitives: Updated Moon::Label

--- lib/moon/packages/render_primitives/ui/label.rb
+++ lib/moon/packages/render_primitives/ui/label.rb
@@ -1,7 +1,7 @@
 require 'render_primitives/render_context'
 
 module Moon
-  # Renderer object for rendering Fonts
+  # Wrapper class around Moon::Text for backwards compatability
   class Label < RenderContext
     # A StringChangedEvent is triggered when the Text#string changes, note
     # this event will trigger even if the String's value is the same.
@@ -41,101 +41,85 @@ module Moon
       end
     end
 
-    # @return [Moon::Vector4]
-    attr_reader   :color
-
-    # @return [Numeric] outline  outline size, 0 or less is disabled
-    attr_reader   :outline
-
-    # @return [Moon::Vector4]
-    attr_accessor :outline_color
-
-    # @return [Moon::Font]
-    attr_reader   :font
-
     # @return [Float]
-    attr_reader   :opacity
+    attr_reader :opacity
 
-    # @return [Symbol] position options are :left, :right, or :center
-    attr_accessor :align
+    # @return [Vector4]
+    attr_reader :color
 
-    # @return [String]
-    attr_reader   :string
-
-    # @return [Float]
-    attr_accessor :line_h
-
-    # @param [String] string
-    # @param [Moon::Font] font
-    def initialize(string = nil, font = nil, align = :left)
-      @lines = []
-      @font = font
-      @align = align
-      @outline = 0
-      @outline_color = Vector4.new(0.0, 0.0, 0.0, 0.0)
-      @line_h = 1.2
-      @opacity = 1.0
+    def initialize(string, font, options = {})
       super()
-      self.color = Vector4.new(1.0, 1.0, 1.0, 1.0)
-      self.string = string
-    end
-
-    def on_opacity_changed(org, cur)
-      refresh_opacity
-    end
-
-    def on_color_changed(org, cur)
+      @opacity = options.fetch(:opacity, 1.0)
+      @text = Moon::Text.new(font, string, options)
+      @color = @text.color.dup
       refresh_color
     end
 
-    def on_outline_changed(org, cur)
-      refresh_size
+    private def refresh_color
+      @render_color = Vector4.new(@color.r, @color.g, @color.b, @color.a)
+      @render_color.a *= @opacity
+      @text.color = @render_color
     end
 
-    def on_font_changed(org, cur)
-      refresh_size
+    def font
+      @text.font
+    end
+
+    def font=(font)
+      @text.font = font
       trigger { FontChangedEvent.new(org, cur) }
     end
 
-    def on_string_changed(org, cur)
-      @lines = @string.split("\n")
-      refresh_size
+    def string
+      @text.string
+    end
+
+    def string=(string)
+      @text.string = string
       trigger { StringChangedEvent.new(org, cur) }
     end
 
-    # @attribute [w] opacity
-    def opacity=(opacity)
-      old = @opacity
-      @opacity = opacity
-      on_opacity_changed old, @opacity
-    end
-
-    # @attribute [w] outline
-    def outline=(outline)
-      old = @outline
-      @outline = outline
-      on_outline_changed old, @outline
-    end
-
-    # @attribute [w] color
     def color=(color)
-      old = @color
       @color = color
-      on_color_changed old, @color
+      refresh_color
     end
 
-    # @attribute [w] font
-    def font=(font)
-      old = @font
-      @font = font
-      on_font_changed old, @font
+    def outline_color
+      @text.outline_color
     end
 
-    # @attribute [w] string
-    def string=(string)
-      old = @string
-      @string = string.to_s
-      on_string_changed old, @string
+    def outline_color=(outline_color)
+      @text.outline_color = outline_color
+    end
+
+    def outline
+      @text.outline
+    end
+
+    def outline=(outline)
+      @text.outline = outline
+    end
+
+    # @return [Symbol] position options are :left, :right, or :center
+    def align
+      @text.align
+    end
+
+    def align=(align)
+      @text.align = align
+      trigger { FontChangedEvent.new(org, cur) }
+    end
+
+    def line_height
+      @text.line_height
+    end
+
+    def line_height=(line_height)
+      @text.line_height = line_height
+    end
+
+    def render_content(x, y, z, options)
+      @text.render x, y, z
     end
 
     # @param [Hash<Symbol, Object>] options
@@ -147,70 +131,6 @@ module Moon
         self.font = fon
       end
       self
-    end
-
-    # @return [Float]
-    private def font_line_h
-      @font.size * @line_h
-    end
-
-    # @param [Integer] x
-    # @param [Integer] y
-    # @param [Integer] z
-    # @param [Hash<Symbol, Object>] options
-    private def render_content(x, y, z, options)
-      if @font && @string
-        @lines.each_with_index do |line, index|
-          case @align
-          when :left
-            # do nothing
-          when :right
-            x -= @font.calc_bounds(line)[0]
-          when :center
-            x -= @font.calc_bounds(line)[0] / 2
-          end
-
-          rc = @render_color
-          if op = options.delete(:opacity)
-            rc = rc.dup
-            rc.a *= op
-          end
-          font.render(x, y + index * font_line_h, z,
-                      line, rc, {
-                        outline: @outline,
-                        outline_color: @outline_color
-                      }.merge(options))
-        end
-      end
-    end
-
-    #
-    private def refresh_size
-      if @font && @string
-        vec2 = Vector2.new(0, 0)
-        @lines.each do |line|
-          bounds = @font.calc_bounds(line)
-          vec2.x = bounds[0] if vec2.x < bounds[0]
-          vec2.y = bounds[1] if vec2.y < bounds[1]
-          vec2.y += [font_line_h, bounds[1]].max
-          vec2.y += @outline # compensate for outline
-          vec2
-        end
-        resize(*vec2.floor)
-      else
-        resize 0, 0
-      end
-    end
-
-    #
-    private def refresh_opacity
-      refresh_color
-    end
-
-    #
-    private def refresh_color
-      @render_color = Vector4.new(@color.r, @color.g, @color.b, @color.a)
-      @render_color.a *= @opacity
     end
   end
 end
