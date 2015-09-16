7132a183f75157654dc3fd9af00a22a8931aa41f
Corey Powell <mistdragon100@gmail.com>
2015-09-16 13:35:26 -0500

    std: Updated Rect
    moved some methods to the core and added comments for the remaining

--- lib/moon/packages/std/rect.rb
+++ lib/moon/packages/std/rect.rb
@@ -1,9 +1,15 @@
 require 'std/core_ext/array'
+require 'std/vector2'
 require 'moon-serializable/load'
 
 module Moon
   class Rect
+    # Helper module for converting objects to Rect parameters [x, y, w, h]
     module Cast
+      # Expands an Array as Rect parameters
+      #
+      # @param [Array] obj
+      # @return [Array<Integer>] rect_parameters [x, y, w, h]
       def self.extract_array(obj)
         case obj.size
         # v2, v2
@@ -16,7 +22,11 @@ module Moon
         end
       end
 
-      def self.extract(obj)
+      # Attempts to extrat the Rect parameters from the obj
+      #
+      # @param [Object] obj
+      # @return [Array<Integer>]
+      def self.try_extract(obj)
         case obj
         when Array   then extract_array(obj)
         when Hash    then obj.fetch_multi(:x, :y, :w, :h)
@@ -24,6 +34,20 @@ module Moon
         when Rect    then obj.to_a
         when Vector2 then return 0, 0, *obj
         when Vector4 then obj.to_a
+        else
+          nil
+        end
+      end
+
+      # Extracts Rect like parameters from the given object
+      #
+      # @param [Object] obj
+      # @return [Array<Integer>]
+      def self.extract(obj)
+        if result = try_extract(obj)
+          return result
+        elsif obj.respond_to?(:to_rect) && (rect = try_extract(obj.to_rect))
+          return rect
         else
           raise TypeError,
                 "wrong argument type #{obj.class.inspect} (expected Array, Hash, Numeric, Rect or Vector2)"
@@ -39,9 +63,9 @@ module Moon
     add_property :w
     add_property :h
 
-    # Splits the current rect into 4 sub rects of half size
+    # Splits the current Rect into 4 sub rects of half its original size
     #
-    # @return [Array[4]<Moon::Rect>]
+    # @return [Array<Moon::Rect>[4]]
     def split
       sw = (w / 2.0).round
       sh = (h / 2.0).round
@@ -51,29 +75,37 @@ module Moon
         Rect.new(x + sw, y + sh, sw, sh)
     end
 
+    # Determines if the Rect contains the given position within its space
+    #
+    # @param [Object] args  a vector2 like object or parameters
+    # @return [Boolean]
     def contains?(*args)
       x, y = *Vector2.extract(args.singularize)
       x.between?(self.x, self.x2 - 1) && y.between?(self.y, self.y2 - 1)
     end
 
-    def clear
-      self.x = 0
-      self.y = 0
-      self.w = 0
-      self.h = 0
-      self
-    end
-
+    # Determines if the Rect is empty, an empty rect either its width or
+    # height as 0
+    #
+    # @return [Boolean]
     def empty?
-      return w == 0 && h == 0
+      w == 0 || h == 0
     end
 
+    # Calculates the intersection between 2 rects
+    #
+    # @param [Array<Integer>, Rect] other
+    # @return [Rect]
     def &(other)
-      nx  = [x, other.x].max
-      ny  = [y, other.y].max
-      Rect.new nx, ny, [x2, other.x2].min - nx, [y2, other.y2].min - ny
+      ox, oy, ow, oh = *other
+      nx  = [x, ox].max
+      ny  = [y, oy].max
+      Rect.new nx, ny, [x2, ox2].min - nx, [y2, oy2].min - ny
     end
 
+    # Sets the Rect's properties from the given args
+    #
+    # @param [Object] args  can be variable number of
     # @return [self]
     def set(*args)
       self.x, self.y, self.w, self.h = *Rect.extract(args.singularize)
@@ -105,6 +137,8 @@ module Moon
       r
     end
 
+    # Scales the Rect's resolution by the given amount
+    #
     # @overload scale(sx, sy)
     #   @param [Float] sx
     #   @param [Float] sy
@@ -115,47 +149,66 @@ module Moon
       r
     end
 
-    def to_s
-      "#{x},#{y},#{w},#{h}"
-    end
-
+    # @return [Hash<Symbol, Integer>]
     def to_h
       { x: x, y: y, w: w, h: h }
     end
 
+    # Returns the right `x` of the Rect
+    #
+    # @return [Integer]
     def x2
       x + w
     end
 
+    # Set's
+    # @param [Integer] x2
     def x2=(x2)
       self.x = x2 - w
     end
 
+    # Returns the bottom `y` of the Rect
+    #
+    # @return [Integer]
     def y2
       y + h
     end
 
+    # @param [Integer] y2
     def y2=(y2)
       self.y = y2 - h
     end
 
+    # The Rect's (x, y) as a Vector2
+    #
+    # @return [Vector2]
     def position
       Vector2.new x, y
     end
 
+    # Sets the Rect's position from the given vector like object
+    #
+    # @param [Object] other  vector2 like object
     def position=(other)
       self.x, self.y = *Vector2.extract(other)
     end
 
+    # The Rect's (w, h) as a Vector2
+    #
+    # @return [Vector2]
     def resolution
       Vector2.new w, h
     end
 
+    # Sets the Rect's resolution from the given vector like object
+    #
+    # @param [Object] other  vector2 like object
     def resolution=(other)
       self.w, self.h = *Vector2.extract(other)
     end
 
-    # Extracts Rect related arguments from the given Object (obj)
+    # Extracts Rect related properties from the given Object (obj).
+    # The Array returned contains [x, y, w, h]
     #
     # @param [Object] obj
     # @return [Array<Integer>]
@@ -163,9 +216,18 @@ module Moon
       Cast.extract(obj)
     end
 
+    # Casts the object to a Rect
+    # If the object is already a Rect, it is returned instead.
+    #
+    # @param [Object] objs
+    # @return [Rect]
     def self.[](*objs)
-      obj = objs.size == 1 ? objs.first : objs
-      new(*extract(obj))
+      obj = objs.singularize
+      if obj.is_a?(Rect)
+        return obj
+      else
+        new(*extract(obj))
+      end
     end
   end
 end
