b730c2213af1e4234de54d2a08ca60bfe3b8c2d5
Corey Powell <mistdragon100@gmail.com>
2015-09-17 12:01:47 -0500

    Added AABB spec

--- spec/moon/packages/std/aabb_spec.rb
+++ spec/moon/packages/std/aabb_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require 'std/aabb'
+require 'std/vector2'
 
 describe Moon::AABB do
   context '#initialize' do
@@ -12,7 +13,33 @@ describe Moon::AABB do
     it 'checks if the aabb intersect with another' do
       aabb1 = described_class.new(0, 4)
       aabb2 = described_class.new(0, 4)
-      aabb1.intersect?(aabb2)
+      # this really should be "to be_intersecting", but uuuuuuugggh
+      expect(aabb1).to be_intersect(aabb2)
+    end
+  end
+
+  context '#&' do
+    it 'creates an intersecting AABB' do
+      a = described_class.new(0, 4)
+      b = described_class.new([2, 2], 4)
+
+      c = a & b
+
+      expect(c.cpos).to eq(Moon::Vector2[1, 1])
+      expect(c.rad).to eq(Moon::Vector2[2, 2])
+    end
+  end
+
+  context '.create_encompassing' do
+    it 'creates an encompassing AABB from the given AABBs' do
+      aabbs = [
+        described_class.new([2, 2], 4),
+        described_class.new([-2, -2], 4)
+      ]
+      result = described_class.create_encompassing(aabbs)
+
+      expect(result.cpos).to eq(Moon::Vector2[0, 0])
+      expect(result.rad).to eq(Moon::Vector2[6, 6])
     end
   end
 end
