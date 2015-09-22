fb404d1276deb4fdabd4f17410abcd3386565007
Corey Powell <mistdragon100@gmail.com>
2015-09-21 22:00:15 -0500

    std: Added doc comment to Integer#masked?

--- lib/moon/packages/std/core_ext/integer.rb
+++ lib/moon/packages/std/core_ext/integer.rb
@@ -1,4 +1,11 @@
 class Integer
+  # Determines if a particular flag or flags have been masked by doing a
+  # binary AND and equal
+  # If the flag is 0, then the integer is tested for zero instead of flag
+  # test
+  #
+  # @param [Integer] flag
+  # @return [Boolean]
   def masked?(flag)
     if flag == 0
       self == 0
