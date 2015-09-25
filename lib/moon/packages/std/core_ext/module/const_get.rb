8430f32e983e0516821d1acc7fdcb77f7b4b7235
Corey Powell <mistdragon100@gmail.com>
2015-09-25 00:45:13 -0500

    std: Updated const_get to accept a second parameter top

--- lib/moon/packages/std/core_ext/module/const_get.rb
+++ lib/moon/packages/std/core_ext/module/const_get.rb
@@ -4,14 +4,15 @@ class Module
   # const_get resolves namespaces and top level constants!
   #
   # @param [String] path
+  # @param [Boolean] inherit
   # @return [Module]
-  def const_get(path)
+  def const_get(path, *inherit)
     top = self
     paths = path.to_s.split("::")
     if path.to_s.start_with?("::")
       top = Object
       paths.shift
     end
-    paths.reduce(top) { |klass, name| klass.__const_get__(name) }
+    paths.reduce(top) { |klass, name| klass.__const_get__(name, *inherit) }
   end
 end
