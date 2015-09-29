3931c78844f75e64caa7b24b0aac145e948028ca
Corey Powell <mistdragon100@gmail.com>
2015-09-29 13:08:39 -0500

    render_primitives: Updated Containable interface
    read comments for more details

--- lib/moon/packages/render_primitives/containable.rb
+++ lib/moon/packages/render_primitives/containable.rb
@@ -1,25 +1,61 @@
 module Moon
   module RenderPrimitive
-    ##
-    # Mixin for containerizing RenderContext objects in a RenderContainer
+    # Interface for allowing containers to
     module Containable
-      # @return [Moon::RenderPrimitive::Containable]
-      attr_accessor :parent
+      # A Containable Parent is an object which will act as the root/parent of
+      # another containable, how the children are remembered by the parent
+      # is solely up to the parent to decide
+      module Parent
+        # This method is called when a child as been adopted by the parent
+        #
+        # @param [Moon::RenderPrimitive::Containable] child
+        # @abstract
+        protected def on_child_adopt(child)
+        end
 
-      ##
-      # Parent containers call this method to invalidate their children
-      # @return [Void]
-      def disown
-        self.parent = nil
+        # Containers call this method to own a child container, if the child had
+        # a parent previously, it will be {#orphan}ed
+        #
+        # @param [Moon::RenderPrimitive::Containable] child
+        # @return [self]
+        # @api public
+        def adopt(child)
+          child.orphan if child.parent
+          child.parent = self
+          on_child_adopt child
+          self
+        end
+
+        # When a child has been disowned this method is called
+        #
+        # @param [Moon::RenderPrimitive::Containable] child
+        # @abstract
+        protected def on_child_disown(child)
+        end
+
+        # Containers call this method to disown a child container
+        #
+        # @param [Moon::RenderPrimitive::Containable] child
+        # @return [self]
+        # @api public
+        def disown(child)
+          if child.parent == self
+            child.parent = nil
+            on_child_disown child
+          end
+          self
+        end
       end
 
-      ##
-      # Wrap this into a RenderContainer
-      # @return [Moon::RenderContainer]
-      def containerize
-        container = RenderContainer.new
-        container.add self
-        container
+      # @return [Moon::RenderPrimitive::Containable::Parent]
+      attr_accessor :parent
+
+      # Child containers call this method to detach themselves from the parent
+      #
+      # @return [self]
+      def orphan
+        parent.disown self if parent
+        self
       end
     end
   end
