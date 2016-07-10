c7dfc67d8b881f0c95fde753323f951c9e9b13f9
Corey Powell <mistdragon100@gmail.com>
2016-07-10 11:08:46 -0500

    serializables: Added load.rb

--- lib/moon/packages/serializables/_basalt.rb
+++ lib/moon/packages/serializables/_basalt.rb
@@ -1,6 +1,1 @@
-require 'serializables/cuboid'
-require 'serializables/rect'
-require 'serializables/vector1'
-require 'serializables/vector2'
-require 'serializables/vector3'
-require 'serializables/vector4'
+require 'serializables/load'
